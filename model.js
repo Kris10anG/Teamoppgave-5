734798a99395f1a32be7900bb6dc71fbeef3aea0
Kris10anG <kristiang@getacademy.no>
2021-09-21 12:17:24 +0200

    ww

--- model.js
+++ model.js
@@ -1,18 +1,6 @@
-// let sounds = [
-//     'Sound1',
-//     'sound2',
-//     'sound3',
-//     'sound4',
-//     'sound5',
-//     'Sound6',
-//     'sound7',
-//     'sound8',
-//     'sound9',
-//     'sound10'
-// ];
-
+let main = document.getElementById("soundboard");
 let sounds = {
-    names: ["en", "to", "tre"],
-    paths: ["sounds/1.mp3", "sounds/2.mp3", "sounds/3.mp3"]
+    names: ["en", "to", "tre", "fire", "fem", "seks", "syv", "åtte", "ni", "ti"],
+    paths: ["sounds/1.mp3", "sounds/2.mp3", "sounds/3.mp3", "sounds/4.mp3", "sounds/5.mp3", "sounds/6.mp3", "sounds/7.mp3", "sounds/8.mp3", "sounds/9.mp3", "sounds/10.mp3"]
 }
 
--- view.js
+++ view.js
@@ -1,26 +1,52 @@
-let soundboard = document.getElementById("soundboard");
+show();
+function show() {
+    let html = '';
+    main.innerHTML = html;
 
-for (let i = 0; i < sounds.names.length; i++) {
-    let soundName = sounds.names[i];
-    let soundPath = sounds.paths[i];
-    let sound = new Audio(soundPath);
+    const wrapper = createElem("div", main, null, "class", "wrapper");
+    const typewriter = createElem("div", wrapper, null, "class", "typewriter");
+    const buttonContainer = createElem("div", typewriter, null, "class", "button-container");
+    const pauseBtn = document.createElement("div");
+    pauseBtn.setAttribute("class", "pause-btn");
+
+    for (let i = 0; i < sounds.names.length; i++) {
+        let soundName = sounds.names[i];
+        let soundPath = sounds.paths[i];
+        let sound = new Audio(soundPath);
+        
+        const btn = createElem("button", buttonContainer, soundName, "class", "big-button");
     
-    const btn = document.createElement("button");
-    btn.innerHTML = soundName;
-    soundboard.appendChild(btn);
+        sound.addEventListener('play', function() { 
+            buttonContainer.classList.add("animate-text");
+            buttonContainer.appendChild(pauseBtn);
+        });
+
+        sound.addEventListener('pause', function() {
+            buttonContainer.classList.remove("animate-text");
+            buttonContainer.removeChild(pauseBtn);
+        });
     
-    const playImg = document.createElement("div");
-    playImg.setAttribute("class", "play-img");
+        btn.addEventListener("click", function() {
+            sound.play();
+        });
+    };
+}
 
-
-    sound.addEventListener('play', function() { 
-       soundboard.appendChild(playImg);
-        console.log("det var get")
-    });
-
-    btn.addEventListener("click", function() {
-        sound.play()
-    });
-};
+/**
+ * Similar to the createElement function, but simplified for this apps purposes.
+ * @param {string} tagName The name of an element.
+ * @param {HTMLElement} parent Parent element for this new child element.
+ * @param {html} html HTML for innerHTML.
+ * @param {string} attrName Example: class, id, width, height..
+ * @param {string} attrValue Example: wrapper, container, left-box..
+ * @returns {HTMLElement}
+ */
+ function createElem(tagName, parent, html = '', attrName, attrValue) {
+    const element = document.createElement(tagName);
+    element.innerHTML = html;
+    attrName ? element.setAttribute(attrName, attrValue) : attrName;
+    parent.appendChild(element);
+    return element;
+}
 
 
